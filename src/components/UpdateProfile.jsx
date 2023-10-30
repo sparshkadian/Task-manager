e72f0ed8b1b189df07d1c2155bacfc7207133f82
Sparsh Kadian <sparshkadian323@gmail.com>
2023-10-30 17:48:59 +0530

    changes to formData

--- src/components/UpdateProfile.jsx
+++ src/components/UpdateProfile.jsx
@@ -2,27 +2,39 @@ import { useState } from 'react';
 import toast from 'react-hot-toast';
 
 const UpdateProfile = () => {
-  const [file, setFile] = useState('');
+  const [formData, setFormData] = useState({ file: '', name: '' });
+  const { file, name } = formData;
 
   const { _id: userId } = JSON.parse(
     window.localStorage.getItem('userDetails')
   );
 
+  const handleChange = (e) => {
+    setFormData((prevValue) => ({
+      ...prevValue,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
+    setFormData((prevValue) => ({
+      ...prevValue,
+      file: e.target.files[0],
+    }));
   };
 
   const handlePhotoUpdate = async (e) => {
     e.preventDefault();
     try {
       if (file === '') throw new Error('No Image selected');
-      const formData = new FormData();
-      formData.append('photo', file);
+      const form = new FormData();
+      form.append('photo', formData.file);
+      form.append('name', formData.name);
       const res = await fetch(
         `http://localhost:4310/api/user/updateMe/${userId}`,
         {
           method: 'PATCH',
-          body: formData,
+          body: form,
         }
       );
       const data = await res.json();
@@ -46,17 +58,34 @@ const UpdateProfile = () => {
         <form onSubmit={handlePhotoUpdate}>
           <div className='mb-5'>
             <label
-              htmlFor='name'
+              htmlFor='file'
               className='mb-3 block text-base font-medium text-[#07074D]'
             >
               Profile Photo
             </label>
             <input
               type='file'
-              name='name'
+              name='file'
               onChange={handleFileChange}
-              id='name'
+              id='file'
               placeholder='Full Name'
+              className='w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md'
+            />
+          </div>
+
+          <div className='mb-5'>
+            <label
+              htmlFor='name'
+              className='mb-3 block text-base font-medium text-[#07074D]'
+            >
+              User Name
+            </label>
+            <input
+              type='text'
+              name='name'
+              onChange={handleChange}
+              value={name}
+              id='name'
               className='w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md'
             />
           </div>
@@ -66,7 +95,7 @@ const UpdateProfile = () => {
               type='submit'
               className='hover:shadow-form w-full rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none'
             >
-              Update Photo
+              Save Changes
             </button>
           </div>
         </form>
